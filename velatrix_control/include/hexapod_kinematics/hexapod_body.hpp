aee94cd8abc0ba7ffc73ae74d366523c5eef53df
Gouri-Shankar-85 <sskzm5585@gmail.com>
2025-11-22 12:41:03 +0530

    modular_cmake

---
+++ velatrix_control/include/hexapod_kinematics/hexapod_body.hpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+*  Filename:           hexapod_body.hpp
+*  Description:        Header defining the transformations for the hexapod body frame and leg frames.
+*  Author:             Gouri Shankar
+******************************************************************************/
+
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+#include "hexapod_kinematics/hexapod_config.hpp"
+
+namespace hexapod_kinematics {
+
+}

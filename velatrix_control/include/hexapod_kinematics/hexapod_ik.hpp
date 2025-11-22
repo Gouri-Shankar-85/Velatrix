f2623d065667a3897b03e0ce3679b2f728371774
Gouri-Shankar-85 <sskzm5585@gmail.com>
2025-11-22 11:35:46 +0530

    hexapod_ik_cmake

---
+++ velatrix_control/include/hexapod_kinematics/hexapod_ik.hpp
@@ -0,0 +1,9 @@
+/******************************************************************************
+*  Filename:           hexapod_ik.hpp
+*  Description:        Header defining hexapod single leg inverse kinematics
+*  Author:             Gouri Shankar
+******************************************************************************/
+
+#pragma once
+
+#include "hexapod_kinematics/hexapod_config.hpp"
